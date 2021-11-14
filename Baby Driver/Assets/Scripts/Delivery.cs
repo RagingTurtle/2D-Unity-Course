d749d878ced22e6f1c1932005b158e81ba5087e1
RagingTurtle <tinybit210r@gmail.com>
2021-11-13 20:37:16 -0600

    Baby changes color when package is picked up
    Course introduces GetComponent method

--- Baby Driver/Assets/Scripts/Delivery.cs
+++ Baby Driver/Assets/Scripts/Delivery.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 0, 0, 1);
     [SerializeField] float packageDestroyDelay = 0.5f;
+    SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = noPackageColor;
+    }
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("bump");
@@ -18,12 +26,13 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package Picked up");
             hasPackage = true;
             Destroy(other.gameObject, packageDestroyDelay);
-
+            spriteRenderer.color = hasPackageColor;
         }
         if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
 
         Debug.Log("enter");
