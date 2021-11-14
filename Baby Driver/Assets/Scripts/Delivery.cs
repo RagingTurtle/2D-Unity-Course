6763090c82e1b7453c0e10039442e05436c76493
RagingTurtle <tinybit210r@gmail.com>
2021-11-13 20:04:46 -0600

    Added Package and Customer
    Course introduced if statements and tags

--- Baby Driver/Assets/Scripts/Delivery.cs
+++ Baby Driver/Assets/Scripts/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other) 
     {
@@ -11,6 +11,15 @@ public class Collision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package Picked up");
+        }
+        if (other.tag == "Customer")
+        {
+            Debug.Log("Package Delivered");
+        }
+
         Debug.Log("enter");
     }
 }
