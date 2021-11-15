2d76a1eacb574c66333c90f3610d9fb48eca51fa
RagingTurtle <tinybit210r@gmail.com>
2021-11-15 08:43:41 -0600

    Disable controls on crash Course introduces scope and public access modifier

--- Snow Boarder/Assets/Scripts/PlayerController.cs
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float torqueAmount =1f;
     [SerializeField] float boostSpeed = 30f;
     [SerializeField] float baseSpeed = 20f;
+    bool canMove = true;
     Rigidbody2D rb2d;
     SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
@@ -20,10 +21,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotatePlayer();
-        RespondToBoost();
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+        
     }
 
+    public void DisableControls()
+    {
+        canMove = false;
+    }
     private void RespondToBoost()
     {
         if (Input.GetKey(KeyCode.W))
