884b8454a05dcc9edfae6629d0f8eab45ab58010
RagingTurtle <tinybit210r@gmail.com>
2021-11-15 13:54:49 -0600

    Added array to store answer added method to access answer

--- Quiz Game/Assets/Scripts/QuestionSO.cs
+++ Quiz Game/Assets/Scripts/QuestionSO.cs
@@ -7,8 +7,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter question here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex=0;
     public string GetQuestion()
     {
         return question;
     }
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+    
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+    
 }
