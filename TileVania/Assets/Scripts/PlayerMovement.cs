0d462f56609b505c8d91d56e35b17be196907197
RagingTurtle <tinybit210r@gmail.com>
2021-11-18 11:42:13 -0600

    Added sprite flip to movement

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 2f;
-Vector2 moveInput;
+    Vector2 moveInput;
     Rigidbody2D rb2d;
     void Start()
     {
@@ -16,6 +16,7 @@ Vector2 moveInput;
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -28,5 +29,14 @@ Vector2 moveInput;
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb2d.velocity.y);
         rb2d.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x), 1f);
+        }
     }
 }
